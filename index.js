3931aa4e8ff26bca563fb3b2ec3908680b0a65ca
Bianaw <bianaw@ac.sce.ac.il>
2024-12-20 17:33:21 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-app.use(express.static(path.join(__dirname)));
-
-app.get('/customerHomePage', (req, res) => {
-    res.sendFile(path.join(__dirname, 'customerHomePage.html'));
-});
